e580ee4d58b70c2f4688113afbe0482e44e06ba2
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-08-26 23:32:18 +0500

    create file

---
+++ index.mjs
@@ -0,0 +1,13 @@
+// Here is the Word Captilizer function which help you to captilize your headings, blogs, titles...
+
+const capitlize = (words) => {
+
+    let data = words.split(" ");
+    let capitalWords = data.map((val) => {
+        return val.charAt(0).toUpperCase() + val.slice(1)
+    });
+
+    return capitalWords.join(" ");
+}
+
+console.log(capitlize("hello world")) // -> Hello World
